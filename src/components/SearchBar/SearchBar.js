9c2252d43ca23dfab9b0b0d66cf5535614a63386
torch <harall09@evergreen.edu>
2019-11-01 15:41:45 -0400

    programmed state management of searchbar components incld search filters & terms; search-submit prints to console

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,29 +1,78 @@
 import React from 'react';
 import './SearchBar.css'
 
-const sortBy_options = {
-    'Best Matched': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviews': 'review_count'
-}
 
 export class SearchBar extends React.Component{
-    render_sortBy_options(){
-        return Object.keys(sortBy_options).map(opt => (<li key={sortBy_options[opt]}> {opt} </li>));
+    constructor(props){
+        super(props);
+        this.state={
+            'searchTerm':'',
+            'location':'',
+            'sortBy':'best_match',
+        }
+
+        this.sortBy_options = {
+            'Best Matched': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviews': 'review_count'
+        }
+        this.handle_termChange = this.handle_termChange.bind(this)
+        this.handle_locationChange = this.handle_locationChange.bind(this)
+        this.handle_submitSearch = this.handle_submitSearch.bind(this)
     }
+
+    get_sortBy_class(opt){
+        return (opt===this.state.sortBy) ? 'active' : '';
+    }
+
+    render_sortBy_options(){
+        return Object.keys(this.sortBy_options).map(opt => {
+            const sortByOption = this.sortBy_options[opt];
+            return (
+                <li
+                    onClick={this.handle_sortBy_change.bind(this, sortByOption)}
+                    className={this.get_sortBy_class(sortByOption)}
+                    key={sortByOption}
+                > {opt} </li>
+                );
+        })
+    }
+
+    handle_sortBy_change(opt){
+        this.setState({'sortBy': opt})
+    }
+
+    handle_termChange(e){
+        this.setState({'searchTerm':e.target.value})
+    }
+
+    handle_locationChange(e){
+        this.setState({'location':e.target.value})
+    }
+
+    handle_submitSearch(e){
+        console.log(this.state)
+        this.searchYelp()
+        e.preventDefault()
+    }
+
+    searchYelp(state){
+        const x = 0;
+    }
+
     render(){
         return (
-            <div className="SearchBar">
+            <div searchYelp={this.searchYelp} className="SearchBar">
                 <div className="SearchBar-sort-options">
                     <ul>
                         {this.render_sortBy_options()}
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input onChange={this.handle_termChange} placeholder="Search Businesses" />
+                    <input onChange={this.handle_locationChange} placeholder="Where?" />
                 </div>
-                <div className="SearchBar-submit">
+                <div onClick={this.handle_submitSearch} className="SearchBar-submit">
                     <a>Let's Go</a>
                 </div>
             </div>
