9dc6c3383b12a7aaba185f9a000cbecfae60062e
torch <harall09@evergreen.edu>
2020-02-05 12:08:44 -0500

    setup gh-pages

---
+++ precache-manifest.129151f74e8e02b3549d18d516ee3575.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91365d38213e0dbdb957619dee1d5b3e",
+    "url": "/react-delish/index.html"
+  },
+  {
+    "revision": "d211db179e90756f4381",
+    "url": "/react-delish/static/css/main.78aefb2d.chunk.css"
+  },
+  {
+    "revision": "05102928628998c32d2f",
+    "url": "/react-delish/static/js/2.b50583d7.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/react-delish/static/js/2.b50583d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d211db179e90756f4381",
+    "url": "/react-delish/static/js/main.ceb8f12e.chunk.js"
+  },
+  {
+    "revision": "3f3da9226ce79c45701b",
+    "url": "/react-delish/static/js/runtime-main.e71040f7.js"
+  },
+  {
+    "revision": "495a729e20bf431b9f396a55d77391f5",
+    "url": "/react-delish/static/media/background_search_desktop.495a729e.jpg"
+  },
+  {
+    "revision": "da9c3d72bd35599a25a839bccc0a3ee7",
+    "url": "/react-delish/static/media/background_search_mobile.da9c3d72.jpg"
+  }
+]);
